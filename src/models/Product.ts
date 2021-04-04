b78de6bcf889638aec422ef5b9202dba5e64d0d7
Pavel Barykin <pavel.barykin@gmail.com>
2021-04-04 05:29:34 +0300

    product model and API path were updated

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -4,11 +4,19 @@ export type Product = {
   id: string,
   title: string,
   description: string,
+  pieces?: number,
+  thumb: string,
+  images: string[],
   price: number,
+  count: number,
 };
 
 export const ProductSchema = Yup.object().shape({
   title: Yup.string().required(),
   description: Yup.string(),
+  pieces: Yup.number(),
+  thumb: Yup.string().required(),
+  images: Yup.array().of(Yup.string()),
   price: Yup.number().required(),
+  count: Yup.number().required(),
 });
