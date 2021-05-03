226d950be29d7cdf195013a8ecb159e836885814
Pavel Barykin <pavel.barykin@gmail.com>
2021-05-03 05:09:13 +0300

    added path for products import API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   products: 'https://f1cwacgkp0.execute-api.eu-west-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://jslh278mdf.execute-api.eu-west-1.amazonaws.com',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
